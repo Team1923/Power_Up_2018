0dcb3601113992ba802c316c7c84afef95a18555
Mayaank Vadlamani <mayaankvad@gmail.com>
2018-01-19 16:19:06 -0500

    Added files needed for todays work session.

---
+++ src/org/usfirst/frc/team1923/commands/auton/DoNothingAuton.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team1923.commands.auton;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Does Nothing
+ */
+public class DoNothingAuton extends Command {
+
+    public DoNothingAuton() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team1923/robot/subsystems/DebugSubsystem.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1923.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Logs match data out to a file on the rio.
+ */
+public class DebugSubsystem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
+}
