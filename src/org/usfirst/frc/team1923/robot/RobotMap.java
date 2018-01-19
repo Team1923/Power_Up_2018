548c8b95bbefefccb19e49991b402a674defa0ff
Mayaank Vadlamani <mayaankvad@gmail.com>
2018-01-19 18:56:29 -0500

    Removed DebugSubSys

--- src/org/usfirst/frc/team1923/robot/RobotMap.java
+++ src/org/usfirst/frc/team1923/robot/RobotMap.java
@@ -1,8 +1,13 @@
 package org.usfirst.frc.team1923.robot;
 
 public class RobotMap {
-	
-	public static final int DRIVER_CONTROLLER_PORT = 0;
-	public static final int OPERATOR_CONTROLLER_PORT = 1;
+
+    public static final boolean DEBUG = true;
+
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
+
+    public static final int[] LEFT_DRIVE_PORTS = {};
+    public static final int[] RIGHT_DRIVE_PORTS = {};
 
 }
