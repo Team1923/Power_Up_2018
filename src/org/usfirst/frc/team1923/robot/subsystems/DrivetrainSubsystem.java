99cbb2455a127c481e1e438ee0c4f06ab275707a
Akshit Dewan <akshit.dewan@yahoo.com>
2018-01-19 19:20:03 -0500

    initial commit

--- src/org/usfirst/frc/team1923/robot/subsystems/DrivetrainSubsystem.java
+++ src/org/usfirst/frc/team1923/robot/subsystems/DrivetrainSubsystem.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1923.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DrivetrainSubsystem extends Subsystem {
@@ -13,31 +14,34 @@ public class DrivetrainSubsystem extends Subsystem {
 
     public DrivetrainSubsystem() {
         // Initialize talons
+        this.leftTalons = new TalonSRX[3];
+        this.rightTalons = new TalonSRX[3];
     }
 
     public void drive(double left, double right) {
-
+        this.leftTalons[0].set(this.leftTalons[0].getControlMode(), left);
+        this.rightTalons[0].set(this.rightTalons[0].getControlMode(), right);
     }
 
     public void setControlMode(ControlMode mode) {
-
+        setControlMode(mode);
     }
 
     public void stop() {
-
+        drive(0, 0);
     }
 
     @Override
     protected void initDefaultCommand() {
-
+        // setDefaultCommand(new RawDriveCommand());
     }
 
-    private double distanceToRotations() {
-        return 0;
+    private double distanceToRotations(double distance) {
+        return distance / (Math.PI * WHEEL_DIAMETER);
     }
 
-    private double rotationsToDistance() {
-        return 0;
+    private double rotationsToDistance(double rotations) {
+        return (Math.PI * WHEEL_DIAMETER) * rotations;
     }
 
 }
