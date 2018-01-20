f90c311b38e2204b85c5227b04f0fc92f57f57f7
James <admin@lyph.io>
2018-01-19 19:00:22 -0500

    Add DrivetrainSubsystem skeleton

--- src/org/usfirst/frc/team1923/robot/subsystems/DrivetrainSubsystem.java
+++ src/org/usfirst/frc/team1923/robot/subsystems/DrivetrainSubsystem.java
@@ -1,4 +1,43 @@
 package org.usfirst.frc.team1923.robot.subsystems;
 
-public class DrivetrainSubsystem {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DrivetrainSubsystem extends Subsystem {
+
+    private static final int WHEEL_DIAMETER = 6; // inches
+
+    private TalonSRX[] leftTalons;
+    private TalonSRX[] rightTalons;
+
+    public DrivetrainSubsystem() {
+        // Initialize talons
+    }
+
+    public void drive(double left, double right) {
+
+    }
+
+    public void setControlMode(ControlMode mode) {
+
+    }
+
+    public void stop() {
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    private double distanceToRotations() {
+        return 0;
+    }
+
+    private double rotationsToDistance() {
+        return 0;
+    }
+
 }
