b78c75fcea8237a44ea495711ddb084884358f9e
Mayaank Vadlamani <mayaankvad@gmail.com>
2018-01-27 17:46:21 -0500

    started elevators; need to finish and fix

--- src/org/usfirst/frc/team1923/robot/RobotMap.java
+++ src/org/usfirst/frc/team1923/robot/RobotMap.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1923.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 public class RobotMap {
 
     public static final int DRIVER_CONTROLLER_PORT = 0;
@@ -7,5 +9,14 @@ public class RobotMap {
 
     public static final int[] LEFT_TALON_PORTS = { 2, 3, 4 };
     public static final int[] RIGHT_TALON_PORTS = { 5, 6, 7 };
+    
+    // TODO: update ports 
+    public static final int LEFT_ELEVATOR_PORT = -1;
+    public static final int RIGHT_ELEVATOR_PORT = -1;
 
+
+    
+    
+    
+    
 }
--- src/org/usfirst/frc/team1923/robot/subsystems/ElevatorSubsystem.java
+++ src/org/usfirst/frc/team1923/robot/subsystems/ElevatorSubsystem.java
@@ -1,12 +1,73 @@
 package org.usfirst.frc.team1923.robot.subsystems;
 
+import org.usfirst.frc.team1923.robot.RobotMap;
+import org.usfirst.frc.team1923.robot.commands.elevator.ElevatorDistanceCommand;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+//need to know more about the hardware 
+//also need to know how to find the "switches" libby talked about 
+// that would allow us to see if the encoder reached a min/max pos
 public class ElevatorSubsystem extends Subsystem {
+	
+	private TalonSRX left;
+	private TalonSRX right;
 
+	private ControlMode controlMode;
+	
+	
+	public ElevatorSubsystem() {
+		
+		left = new TalonSRX(RobotMap.LEFT_ELEVATOR_PORT);
+		right = new TalonSRX(RobotMap.RIGHT_ELEVATOR_PORT);
+		this.controlMode = ControlMode.PercentOutput;
+		
+		// resetEncoders();
+		
+	}
+	
+	
+	public void raise(double power) {
+		// TODO check position of elevator 
+		// if position is max, dont do anything
+		
+		this.left.set(this.controlMode, power);
+		this.right.set(this.controlMode, power);
+	}
+	
+	
+	public void lower(double power ) {
+		// TODO check position of elevator 
+		// if position is min, dont do anything
+		
+		this.left.set(this.controlMode, -power);
+		this.right.set(this.controlMode, power);
+	}
+
+	public void resetEncoders() {
+		
+	}
+	
+	public double getElevatorPosition() {
+		return 0.0;
+		
+	}
+	
+	// stops the elevator at whatever position its at
+	public void stop() {
+		this.left.set(this.controlMode, 0);
+		this.right.set(this.controlMode, 0);
+	}
+	
+	// TODO fix
     @Override
     protected void initDefaultCommand() {
 
+		setDefaultCommand(new ElevatorDistanceCommand(0));
     }
 
 }
