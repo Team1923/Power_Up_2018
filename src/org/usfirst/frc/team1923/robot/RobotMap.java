e1c723202fee299879bae3a7c9f5a8efd3177a34
RohitMTheOne <rohitmukund11@gmail.com>
2018-01-27 15:26:50 -0500

    Finished Intake Subsystem

--- src/org/usfirst/frc/team1923/robot/RobotMap.java
+++ src/org/usfirst/frc/team1923/robot/RobotMap.java
@@ -8,4 +8,15 @@ public class RobotMap {
     public static final int[] LEFT_TALON_PORTS = { 2, 3, 4 };
     public static final int[] RIGHT_TALON_PORTS = { 5, 6, 7 };
 
+    public static final int LEFT_INTAKE_TALON_PORT = 8;
+    public static final int RIGHT_INTAKE_TALON_PORT = 9;
+
+    public static final int INTAKE_ULTRASONIC_PING_PORT = 10;
+    public static final int INTAKE_ULTRASONIC_ECHO_PORT = 11;
+    
+    public static final int LEFT_DOUBLESOLENOID_FORWARD_PORT = 12;
+    public static final int LEFT_DOUBLESOLENOID_REVERSE_PORT = 13;
+    public static final int RIGHT_DOUBLESOLENOID_FORWARD_PORT = 14;
+    public static final int RIGHT_DOUBLESOLENOID_REVERSE_PORT = 15;
+
 }
