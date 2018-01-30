8f3f838c9bf965f64d464be43a41c20bef3e6586
RohitMTheOne <rohitmukund11@gmail.com>
2018-01-30 18:39:42 -0500

    Elevator Position Command coded - Rohit Mukund, Abhi Vellore, Rajiv Chatterjee

--- src/org/usfirst/frc/team1923/robot/commands/elevator/ElevatorPositionCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/elevator/ElevatorPositionCommand.java
@@ -1,36 +1,54 @@
 package org.usfirst.frc.team1923.robot.commands.elevator;
 
+import com.ctre.phoenix.*;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.usfirst.frc.team1923.robot.Robot;
+import org.usfirst.frc.team1923.robot.RobotMap;
+import org.usfirst.frc.team1923.robot.subsystems.DrivetrainSubsystem;
+import org.usfirst.frc.team1923.robot.subsystems.ElevatorSubsystem;
 
 /**
  * Move the elevator to a set position
  */
 public class ElevatorPositionCommand extends Command {
+	private double position;
+	private double target;
+	private int targetEncoderTick;
+	ControlMode controlMode;
 
-    public ElevatorPositionCommand() {
-        this.requires(Robot.elevatorSubsystem);
-    }
+	public ElevatorPositionCommand(double position) {
+		this.requires(Robot.elevatorSubsystem);
+		this.position = position;
+		SmartDashboard.putNumber("position to run", position);
 
-    protected void initialize() {
+	}
 
-    }
+	protected void initialize() {
+		this.position = SmartDashboard.getNumber("position to run", position);
+		this.targetEncoderTick = (int) (position * 4000); // to do
+		Robot.elevatorSubsystem.set(ControlMode.MotionMagic, this.targetEncoderTick);
 
-    protected void execute() {
+	}
 
-    }
+	protected void execute() {
+		// Only for debugging purposes
+	}
 
-    protected boolean isFinished() {
-        return Robot.oi.operator.getLeftY() > 0.3;
-    }
+	protected boolean isFinished() {
+		return (Math.abs(position - this.targetEncoderTick) < 300 && Math.abs(position - this.targetEncoderTick) < 300);
+	}
 
-    protected void end() {
-        Robot.elevatorSubsystem.stop();
-    }
+	protected void end() {
+		Robot.elevatorSubsystem.stop();
+	}
 
-    protected void interrupted() {
-        this.end();
-    }
+	protected void interrupted() {
+		this.end();
+	}
 
 }
