1d3450c73c640feed45808ca4a0f72eebf2f5323
Parth Shastri <31370288+cppio@users.noreply.github.com>
2018-02-02 16:27:46 -0500

    cleanup elevator

--- src/org/usfirst/frc/team1923/robot/subsystems/ElevatorSubsystem.java
+++ src/org/usfirst/frc/team1923/robot/subsystems/ElevatorSubsystem.java
@@ -1,10 +1,10 @@
 package org.usfirst.frc.team1923.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.ParamEnum;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -15,15 +15,14 @@ import org.usfirst.frc.team1923.robot.commands.elevator.ElevatorZeroCommand;
 
 public class ElevatorSubsystem extends Subsystem {
 
-    private final double K_P = 0.2000;
-    private final double K_I = 0.0001;
-    private final double K_D = 0.0000;
-    private final double K_F = 0.0000;
+    private static final double K_P = 0.2000;
+    private static final double K_I = 0.0001;
+    private static final double K_D = 0.0000;
+    private static final double K_F = 0.0000;
 
-    private final int ALLOWABLE_ERROR = 300;
+    private static final int ALLOWABLE_ERROR = 300;
 
     private TalonSRX[] talons;
-    private boolean zeroed;
 
     public ElevatorSubsystem() {
         this.talons = new TalonSRX[RobotMap.ELEVATOR_TALON_PORTS.length];
@@ -46,7 +45,7 @@ public class ElevatorSubsystem extends Subsystem {
         this.talons[0].configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, RobotMap.TALON_COMMAND_TIMEOUT);
         this.talons[0].configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, RobotMap.TALON_COMMAND_TIMEOUT);
 
-        this.talons[0].configSetParameter(ParamEnum.eClearPositionOnLimitR, 1, 0x00, 0x00, RobotMap.TALON_COMMAND_TIMEOUT);
+        this.talons[0].configSetParameter(ParamEnum.eClearPositionOnLimitR, 1, 0, 0, RobotMap.TALON_COMMAND_TIMEOUT);
 
         this.talons[0].configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, RobotMap.TALON_COMMAND_TIMEOUT);
 
@@ -55,8 +54,6 @@ public class ElevatorSubsystem extends Subsystem {
         this.talons[0].config_kD(0, K_D, RobotMap.TALON_COMMAND_TIMEOUT);
         this.talons[0].config_kF(0, K_F, RobotMap.TALON_COMMAND_TIMEOUT);
         this.talons[0].configAllowableClosedloopError(0, ALLOWABLE_ERROR, RobotMap.TALON_COMMAND_TIMEOUT);
-
-        this.zeroed = this.talons[0].getSensorCollection().isRevLimitSwitchClosed();
     }
 
     public void stop() {
@@ -68,12 +65,11 @@ public class ElevatorSubsystem extends Subsystem {
     }
 
     public boolean isZeroed() {
-        return this.zeroed;
+        return this.talons[0].getSensorCollection().isRevLimitSwitchClosed();
     }
 
     public void zero() {
-        ElevatorZeroCommand zeroCommand = new ElevatorZeroCommand();
-        zeroCommand.start();
+        new ElevatorZeroCommand().start();
     }
 
     @Override
