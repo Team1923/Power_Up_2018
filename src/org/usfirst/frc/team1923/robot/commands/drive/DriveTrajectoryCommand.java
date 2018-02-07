cfa740dabbf8ecc2dded81056e59ee3160c7d109
coder3462 <rahuldshah3@gmail.com>
2018-02-07 00:01:13 -0500

    updated Pathfinder

--- src/org/usfirst/frc/team1923/robot/commands/drive/DriveTrajectoryCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/drive/DriveTrajectoryCommand.java
@@ -7,74 +7,95 @@ import jaci.pathfinder.Trajectory.FitMethod;
 import jaci.pathfinder.Trajectory.Config;
 import jaci.pathfinder.Waypoint;
 import jaci.pathfinder.modifiers.TankModifier;
+import edu.wpi.first.wpilibj.command.Command;
 
-public class DriveTrajectoryCommand {
+public class DriveTrajectoryCommand extends Command {
 	
-    static boolean libLoaded = false;
+	public FitMethod fit = Trajectory.FitMethod.HERMITE_CUBIC; 	// Fit Method:  HERMITE_CUBIC or HERMITE_QUINTIC
+	public int samples = Trajectory.Config.SAMPLES_HIGH;
+	public double timeStep = 0.05;
+	static boolean libLoaded;
+	Trajectory trajectory;
+	Waypoint[] points;
+	public double maxVelocity = 1.7; // in m/s
+    public double maxAcceleration = 2.0; // in m/s/s
+    public double maxJerk = 60.0; // in m/s/s/s
+    Trajectory.Config configuration;
+    
+    public Waypoint[] DRIVE_STRAIGHT_5M = new Waypoint[] {
+            new Waypoint(0, 0, 0),
+            new Waypoint(5, 0, 0),
+    };
+    
+    public Waypoint[] LEFT_SWITCH_AUTON = new Waypoint[] {
+  		  new Waypoint(0, 0, 0),
+  		  new Waypoint(5, -14, 90),
+    };
+    
+    public Waypoint[] RIGHT_SWITCH_AUTON = new Waypoint[] {
+    		  new Waypoint(0, 0, 0),
+    		  new Waypoint(5, 14, -90),
+      };
+    
+	static {
+		if (libLoaded == false) {
+			try {
+				System.loadLibrary("pathfinderjava");
+				libLoaded = true;
+			} 
+			catch (Exception e) {
+				// TODO make this go to Shuffleboard
+				System.out.println("ERROR LOADING PATHFINDER!\nPATHFINDER IS NOT GOING TO WORK!\n" + "Error: ");
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+    public DriveTrajectoryCommand(Waypoint[] points) {
+    	
+    	this.points = points;
+        
+        configuration = new Trajectory.Config(fit, samples, timeStep, maxVelocity, maxAcceleration, maxJerk); 
+        trajectory = Pathfinder.generate(points, configuration);
+        
+        for (int i = 0; i < trajectory.length(); i++) {
+    	    Trajectory.Segment seg = trajectory.get(i);
+    	    
+    	    System.out.printf("%f,%f,%f,%f,%f,%f,%f,%f\n", 
+    	        seg.dt, seg.x, seg.y, seg.position, seg.velocity, 
+    	            seg.acceleration, seg.jerk, seg.heading);
+    	}
+        
+        //TODO set waypoints
+        
+     // The distance between the left and right sides of the wheelbase is 0.6m
+    	double wheelbase_width = 0.6;
 
-    static {
-        if (!libLoaded) {
-            try {
-                System.loadLibrary("pathfinderjava");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            libLoaded = true;
-        }
+    	// Create the Modifier Object
+    	TankModifier modifier = new TankModifier(trajectory);
+
+    	// Generate the Left and Right trajectories using the original trajectory
+    	// as the centre
+    	modifier.modify(wheelbase_width);
+
+    	Trajectory left  = modifier.getLeftTrajectory();       // Get the Left Side
+    	Trajectory right = modifier.getRightTrajectory();      // Get the Right Side
+    	
     }
     
-	/**
-	 * x         The X position of the waypoint in meters
-     * y         The Y position of the waypoint in meters
-     * angle     The exit angle of the waypoint in degrees
-	 */
-    // TODO set waypoints
-	double x1, y1;
-	double x2, y2;
-	double x3, y3;
-	double angle1, angle2, angle3;
-	
-	Waypoint[] points = new Waypoint[] {
-			new Waypoint(x1, y1, Pathfinder.d2r(angle1)),
-			new Waypoint(x2, y3, Pathfinder.d2r(angle2)),
-			new Waypoint(x3, y3, Pathfinder.d2r(angle2)),
-	};
-
-	// Create the Trajectory Configuration
-	// Fit Method:  HERMITE_CUBIC or HERMITE_QUINTIC
-    public FitMethod fit = Trajectory.FitMethod.HERMITE_CUBIC;
-    // Sample Count:        SAMPLES_HIGH (100 000)
-    //  					SAMPLES_LOW  (10 000)
-    //  					SAMPLES_FAST (1 000)
-    public int samples = Trajectory.Config.SAMPLES_HIGH;
-    public double timeStep = 0.05; 
-    public double max_velocity = 1.7; // in m/s
-    public double max_acceleration = 2.0; // in m/s/s
-    public double max_jerk = 60.0; // in m/s/s/s
-	Trajectory.Config config = new Trajectory.Config(fit, samples, timeStep, max_velocity, max_acceleration, max_jerk); 
-	
-	Trajectory trajectory = Pathfinder.generate(points, config);{
-	
-	for (int i = 0; i < trajectory.length(); i++) {
-	    Trajectory.Segment seg = trajectory.get(i);
-	    
-	    System.out.printf("%f,%f,%f,%f,%f,%f,%f,%f\n", 
-	        seg.dt, seg.x, seg.y, seg.position, seg.velocity, 
-	            seg.acceleration, seg.jerk, seg.heading);
+    @Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
 	}
-	
-	// The distance between the left and right sides of the wheelbase is 0.6m
-	double wheelbase_width = 0.6;
-
-	// Create the Modifier Object
-	TankModifier modifier = new TankModifier(trajectory);
-
-	// Generate the Left and Right trajectories using the original trajectory
-	// as the centre
-	modifier.modify(wheelbase_width);
-
-	Trajectory left  = modifier.getLeftTrajectory();       // Get the Left Side
-	Trajectory right = modifier.getRightTrajectory();      // Get the Right Side
-	
-	}
+    
+    public Trajectory getTrajectory() {
+    	return trajectory;
+    }
+    
+    public void genTrajectory(Waypoint[] waypoints) {
+    	Pathfinder.generate(waypoints, configuration);
+    }
+ 
 }
